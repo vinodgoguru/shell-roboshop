eb7ab1584d4aa43aeffdee0e3b60dd2025d71bed
Vinod <gvinodkumar900@gmail.com>
2026-05-26 15:12:13 +0530

    fifth commit

---
+++ 01-mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIME_STAMP [ERROR] $R Please run this script with root user $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIME_STAMP [ERROR] $2 ....$R FAILUER $N" | tee -a $LOGS_FILE
+        exit 1
+    else    
+        echo -e "$TIME_STAMP [ERROR] $2 ....$G SUCCESS $N" | tee -a $LOGS_FILE
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo Repo"
+
