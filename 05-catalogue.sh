4f362a7002f5b619733039c8af48865150474da6
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-27 19:06:24 +0530

    fifth commit

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -68,7 +68,7 @@ VALIDATE $? "Added Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host mongodb.daws90s.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.vijaansh.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -lt 0 ]; then
     mongosh --host mongodb.daws90s.shop </app/db/master-data.js &>>$LOGS_FILE
